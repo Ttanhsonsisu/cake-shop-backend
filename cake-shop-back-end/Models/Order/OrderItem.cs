7dc7ea3f5f4c4b75e5fd2db69d5c4b141d2a7b3b
anhtt <ttanhsonsisu@gmail.com>
2025-11-19 14:42:22 +0700

    ADD feature order in cms site

--- cake-shop-back-end/Models/Order/OrderItem.cs
+++ cake-shop-back-end/Models/Order/OrderItem.cs
@@ -11,10 +11,5 @@ public class OrderItem : MasterCommonModel
     public int quantity { get; set; } = 1;
     public decimal price { get; set; }
 
-    // Lưu ý: total_price là cột tính toán (AS PERSISTED) trong SQL.
-    // Trong C# Model, ta thường định nghĩa nó là thuộc tính chỉ đọc (readonly) 
-    // hoặc tính toán lại giá trị dựa trên các thuộc tính khác.
-    // Nếu sử dụng Entity Framework, bạn có thể cấu hình nó là cột tính toán 
-    // trong DbContext (OnModelCreating) hoặc loại trừ khỏi mapping (Ignore).
     public decimal total_price { get; set; }
 }
